661118fa4a249258295dd1186c9f723c5528eb47
BAKHEET\yuliming <yuliming@phc-dow.com>
2017-12-15 18:02:04 +0800

    Initial commit

---
+++ src/main/java/com/dawn/apple/turbine/TurbineApplication.java
@@ -0,0 +1,19 @@
+package com.dawn.apple.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author dawn
+ */
+@SpringBootApplication
+@EnableTurbine
+@EnableDiscoveryClient
+public class TurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+}
